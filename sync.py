1999adf3d2df71bd03a21c5a05f5cfe7010cdaf2
Karen Xie <kxie094@aucklanduni.ac.nz>
2015-09-14 13:52:07 +1200

    Can now load a json file and check if there's an entry for a file in there.

--- sync.py
+++ sync.py
@@ -33,20 +33,7 @@ def sync_dirs(from_dir, to_dir):
     from_sync_file = from_dir + "/.sync"
     to_sync_file = from_dir + "/.sync"
 
-    # with open(from_dir + "/.sync", "a+") as f:
-    #     data = {
-    #         "file1_2.txt": [
-    #             [
-    #              "2015-08-26 16:53:53 +1200",
-    #              "deleted"
-    #             ],
-    #             [
-    #             "2015-08-26 16:52:56 +1200",
-    #             "a8299b688d515f86123fcb56b797f9a10988d5c359f3b99734ca43f67913887d"
-    #             ]
-    #         ]
-    #     }    
-    #     json.dump(data, f, indent=4)
+    #write_json_to_file('file1_2.txt', [[12, 12],[33,33]], from_sync_file)
 
     files = get_files_in_dir(from_dir)
     if not files:
@@ -54,9 +41,24 @@ def sync_dirs(from_dir, to_dir):
     for f in files:
         print(f)
 
+    update_sync_file(from_dir)
+
 def update_sync_file(directory):
     """Scans a given directory for files and updates its sync file."""
-    print(directory)
+
+    # Get files in directory
+    files = get_files_in_dir(directory)
+
+    sync_file = directory + "/.sync"
+    print(sync_file)
+
+    # Load sync file
+    with open(sync_file, "r+") as sync_f:
+        data = json.load(sync_f)
+        for f in files:
+            if f in data:
+                print('FOUND YOU')
+    return files
 
 def get_files_in_dir(directory):
     """Gets files, excluding dirs, inside a given directory as a list."""
@@ -65,5 +67,10 @@ def get_files_in_dir(directory):
         if not f.startswith('.'):
             files.append(f)
     return files
+
+def write_json_to_file(key, value, json_file):
+    """Writes a key value pair to a json file."""
+    with open(json_file, "a+") as f:
+        json.dump({key: value}, f, indent=4, separators=(',', ': '))
 
 do_sync(sys.argv[1], sys.argv[2])
